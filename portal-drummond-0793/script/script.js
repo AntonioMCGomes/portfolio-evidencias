a5f8aee7e5a99fb52711e334902c20eb1eec9bff
AntonioMCGomes <marxmarc@gmail.com>
2026-09-11 13:22:44 +0100

    portal-drummond

---
+++ portal-drummond-0793/script/script.js
@@ -0,0 +1,13 @@
+
+/*Texto interativo
+const textos = document.querySelectorAll('.texto');
+
+textos.forEach(text => {
+    text.innerHTML = text.textContent
+        .split('')
+        .map(char => `<span>${char}</span>`)
+        .join('');
+});
+
+*/
+
